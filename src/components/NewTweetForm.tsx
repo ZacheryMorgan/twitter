b81dea8e7760ac4191c744daa57724d4b3f86a3a
ZacheryShawne <zachery.shawne@gmail.com>
2023-05-18 19:19:33 -0400

    send tweet to database

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,7 +1,14 @@
 import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
-import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import {
+  FormEvent,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { api } from "~/utils/api";
 
 const updateTextAreaSize = (textArea?: HTMLTextAreaElement) => {
   if (textArea == null) return;
@@ -30,9 +37,24 @@ const Form = () => {
     updateTextAreaSize(textAreaRef.current);
   }, [inputValue]);
 
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      setInputValue("");
+    },
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    createTweet.mutate({ content: inputValue });
+  };
+
   if (session.status !== "authenticated") return null;
   return (
-    <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+    <form
+      onSubmit={handleSubmit}
+      action=""
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
       <div>
         <div className="flex gap-4">
           <ProfileImage src={session.data.user.image} />
